67a88713e4947fce9505f65ad37b2c6583e63f56
Johannes Wikner <johannes.wikner@gmail.com>
2013-08-09 12:15:16 +0200

    new bundles

--- system/path.zsh
+++ system/path.zsh
@@ -4,5 +4,6 @@ export PATH="$PATH:/usr/local/bin"
 export PATH="$PATH:/usr/local/sbin"
 export PATH="$PATH:$HOME/.sfs"
 export PATH="$PATH:$ZSH/bin"
-
+export PATH="$PATH:$HOME/src/android-sdks/platform-tools"
+export PATH="$PATH:$HOME/src/android-sdks/tools"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"

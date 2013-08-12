75493e61c720af3ae83726d070d7dcacc4ff3e81
Johannes Wikner <johannes.wikner@gmail.com>
2013-08-12 17:11:43 +0200

    xmonad and vimrc hacking

--- xmonad/xmonad.symlink/xmonad.hs
+++ xmonad/xmonad.symlink/xmonad.hs
@@ -1,10 +1,15 @@
 import XMonad
-import XMonad.Util.Run(spawnPipe)
+import qualified XMonad.StackSet as W
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
 import XMonad.Layout.Tabbed
 import XMonad.Layout.NoBorders
+import XMonad.Layout.Fullscreen
+import XMonad.Util.Run(spawnPipe)
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
+import XMonad.Util.NamedScratchpad
 import System.IO
 
 
@@ -25,14 +30,28 @@ myTiled = smartBorders $ Tall nmaster delta ratio
         delta = 3 / 100
         ratio = 1 / 2
 
-myTabbed = tabbed shrinkText tabTheme
+myLayout = smartBorders 
+    $ mkToggle (NOBORDERS ?? FULL ?? EOT) 
+    $ avoidStruts 
+    $ myTiled ||| Full 
 
-myLayout = avoidStruts $ smartBorders $ 
-            myTabbed 
-            ||| myTiled 
-            ||| Mirror myTiled 
-            ||| Full
+------------------------------
+-- SCRATCHPADS
+------------------------------
 
+myScratchpads = let 
+  reallyFull = customFloating $ W.RationalRect 0.025 0.025 0.95 0.95
+  full = customFloating $ W.RationalRect 0.05 0.05 0.9 0.9
+  top = customFloating $ W.RationalRect 0.025 0.05 0.95 0.45
+  bottom = customFloating $ W.RationalRect 0.2 0.7 0.60 0.3
+  in [
+    NS "Mail" 
+       "google-chrome --app=https://mail.google.com"
+       (appName =? "mail.google.com") full 
+  , NS "BottomTerminal"
+       "urxvtc -name BottomTerminal"
+       (appName =? "BottomTerminal") bottom 
+  ]
 ------------------------------
 -- THEME
 ------------------------------
@@ -82,40 +101,59 @@ baseTheme = defaultTheme
     , decoHeight            = 22
     }
 
-tabTheme :: Theme
-tabTheme = baseTheme
-    { -- base00, base01, blue all good activeColors
-      activeColor           = base03
-    , activeBorderColor     = base03
-    , activeTextColor       = base00
-    }
+myKeys = myModKeys ++ myFnKeys 
+  where
+    myModKeys =
+      let binds =
+            [ (xK_p, spawn "exec dmenu-dark -b")
+            , (xK_g, spawn "exec google-chrome")
+            , (xK_b, namedScratchpadAction myScratchpads "BottomTerminal")
+            , (xK_m, namedScratchpadAction myScratchpads "Mail")
+            , (xK_Caps_Lock, sendMessage $ Toggle FULL)
+            ]
+      in [((myModMask, key), action) | (key, action) <- binds]
+    myFnKeys = 
+       -- XF86AudioMute
+      [ ((0 , 0x1008FF12), spawn "amixer -q set Master 0")
+      -- XF86AudioLowerVolume
+      , ((0 , 0x1008ff11), spawn "amixer -q set Master 10%- unmute && amixer -q set Headphone unmute && amixer -q set Speaker unmute")
+      -- XF86AudioRaiseVolume
+      , ((0 , 0x1008ff13), spawn "amixer -q set Master 10%+ unmute && amixer -q set Headphone unmute && amixer -q set Speaker unmute")
+      ]
 
-myKeys =
-     -- XF86AudioMute
-    [ ((0 , 0x1008FF12), spawn "amixer -q set Master 0")
-    -- XF86AudioLowerVolume
-    , ((0 , 0x1008ff11), spawn "amixer -q set Master 10%- unmute && amixer -q set Headphone unmute && amixer -q set Speaker unmute")
-    -- XF86AudioRaiseVolume
-    , ((0 , 0x1008ff13), spawn "amixer -q set Master 10%+ unmute && amixer -q set Headphone unmute && amixer -q set Speaker unmute")
-    ]
-myKeysP = 
-    [ ("M-p", spawn "exec dmenu-dark -b")
-    , ("M-g", spawn "exec google-chrome")
-    ]
+myManageHook = manageDocks 
+            <+> manageHook defaultConfig 
+            <+> namedScratchpadManageHook myScratchpads
+myPP = defaultPP
+    { ppCurrent             = xmobarColor base02 blue . wrap " " " "
+    , ppTitle               = xmobarColor base0 "" . shorten 200
+    , ppVisible             = wrap "(" ")"
+    , ppUrgent              = xmobarColor base02 yellow . wrap " " " "
+    , ppHidden              = id
+    , ppHiddenNoWindows     = const ""
+    , ppSep                 = "  "
+    , ppWsSep               = " "
+    , ppLayout              = const ""
+    , ppOrder               = id
+    , ppSort                = fmap 
+                              (namedScratchpadFilterOutWorkspace.)
+                              (ppSort defaultPP)
+    , ppExtras              = []
+    }
+  
 main = do
     xmproc <- spawnPipe "/home/jnes/.cabal/bin/xmobar /home/jnes/.xmobarrc"
     xmonad $ defaultConfig 
-        {  manageHook = manageDocks <+> manageHook defaultConfig
-        ,  layoutHook = myLayout
-      	,  logHook = dynamicLogWithPP xmobarPP 
-            {  ppOutput = hPutStrLn xmproc
-		    ,  ppTitle = xmobarColor red "" . shorten 60
-		    }
-        ,  terminal = myTerminal
-        ,  modMask = myModMask
-        ,  borderWidth = myBorderWidth
-        ,  normalBorderColor = myNormalBorderColor
+        {  layoutHook         = myLayout
+      	,  logHook            = dynamicLogWithPP myPP
+            {  ppOutput       = hPutStrLn xmproc
+            }
+        ,  terminal           = myTerminal
+        ,  modMask            = myModMask
+        ,  borderWidth        = myBorderWidth
+        ,  normalBorderColor  = myNormalBorderColor
         ,  focusedBorderColor = myFocusedBorderColor
-        ,  focusFollowsMouse = myFocusFollowsMouse
-        } `additionalKeys` myKeys `additionalKeysP` myKeysP 
+        ,  focusFollowsMouse  = myFocusFollowsMouse
+        ,  manageHook         = myManageHook
+        } `additionalKeys` myKeys 
       
